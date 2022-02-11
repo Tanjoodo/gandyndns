426a535f26ff58c84b091a882bc531926149198d
Mohammed Arabiat <tanjoodo@gmail.com>
2022-02-11 21:11:59 +0200

    Fix the API to use PUT instead of POST and adjust body accordingly.
    POST no longer allows updating the domain name. PUT requires all values
    to be wrapped in an array at the root of the body named "items".

--- update_dyndns.sh
+++ update_dyndns.sh
@@ -4,5 +4,5 @@ source /etc/dyndns.config
 
 ip=$(curl -s ifconfig.co/)
 
-curl -D- -X POST -H "Content-Type: application/json" -H "X-Api-Key: $APIKEY" -d "{\"rrset_name\":\"$SUBDOMAIN\", \"rrset_type\":\"A\", \"rrset_ttl\":\"$TTL\", \"rrset_values\":[\"$ip\"]}" $RECORD_ENDPOINT
+curl -D- -X PUT -H "Content-Type: application/json" -H "X-Api-Key: $APIKEY" -d "{\"items\":[{\"rrset_name\":\"$SUBDOMAIN\", \"rrset_type\":\"A\", \"rrset_ttl\":\"$TTL\", \"rrset_values\":[\"$ip\"]}]}" $RECORD_ENDPOINT
 
